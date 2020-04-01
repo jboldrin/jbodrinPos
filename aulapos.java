48bc22141e62eab06ba6679cecd714ead488aaf6
jboldrin <62973443+jboldrin@users.noreply.github.com>
2020-03-31 21:52:18 -0400

    Add files via upload

---
+++ aulapos.java
@@ -0,0 +1,7 @@
+class MeuPrograma {
+public static void main(String[] args) {
+// miolo do programa começa aqui!
+System.out.println("Minha primeira aplicação Java!!");
+// fim do miolo do programa
+}
+}
